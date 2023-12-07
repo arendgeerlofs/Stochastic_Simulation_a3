219574718ed9702922a4564e8f1b23cf0b0484cd
Mirjamkdk <122541036+Mirjamkdk@users.noreply.github.com>
2023-12-07 13:55:05 +0100

    created files

---
+++ functions.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  7 13:54:18 2023
+
+@author: mirja
+"""
---
+++ run.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  7 13:54:44 2023
+
+@author: mirja
+"""
