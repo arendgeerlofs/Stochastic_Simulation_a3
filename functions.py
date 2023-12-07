271eb4fc465a83a3df5b0ef670846710b02bde48
arendgeerlofs <arendgeerlofs@hotmail.nl>
2023-12-07 14:27:50 +0100

    Hill climbing

--- functions.py
+++ functions.py
@@ -1,6 +1,23 @@
-# -*- coding: utf-8 -*-
 """
-Created on Thu Dec  7 13:54:18 2023
+Functions file
+"""
+import numpy as np
 
-@author: mirja
-"""
+
+def hill_climbing(params, data, objective_function, iterations, step_size = 1):
+    """
+    Local optimum optimization function that converges to local optimum by slightly
+    changing param values randomly and comparing objective scores
+    """
+    best_score = objective_function(params, data)
+    best_params = params
+    for _ in range(iterations):
+        # Calculate score of deviation
+        deviation = step_size * np.random.randn(len(params))
+        params = best_params + deviation
+        score = objective_function(params, data)
+        # Change params and score if closer to local optimum
+        if score > best_score:
+            best_params = params
+            best_score = score
+    return best_params
--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
 """
-Created on Thu Dec  7 13:54:44 2023
-
-@author: mirja
+Run file
 """
