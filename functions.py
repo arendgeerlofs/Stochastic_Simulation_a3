e9192277a61bd434ba3aa8fffd2f188d66d19e92
Ksf900 <kevin.schaaf@student.uva.nl>
2023-12-07 14:03:52 +0100

    Objective function nr. 1

--- functions.py
+++ functions.py
@@ -4,3 +4,11 @@ Created on Thu Dec  7 13:54:18 2023
 
 @author: mirja
 """
+
+import numpy as np
+
+def mean_squared_error(data, simulated_data):
+    '''
+    Mean squared error (MSE) as objective function number 1
+    '''
+    return np.mean((data - simulated_data)**2)
