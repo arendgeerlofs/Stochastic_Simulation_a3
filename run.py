36e0649fa45ce470323cc66efefdb3317e8d5989
Mirjamkdk <122541036+Mirjamkdk@users.noreply.github.com>
2023-12-08 13:16:51 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,40 @@
 """
 Run file
 """
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+from functions import *
+
+# Importing data
+data = np.loadtxt('predator-prey-data.csv', skiprows=1, delimiter=',')
+
+data_xy = data[:, 2:4]
+data_t = data[:, 1]
+dt = data_t[1]
+
+# Plotting data
+plt.plot(data_t, data_xy, 'o')
+plt.legend(['x', 'y'])
+plt.xlabel('Time')
+plt.ylabel('Population size')
+plt.title('Predator-prey data')
+plt.show()
+
+# Initial parameters
+init_params = [0, 0, 2, 1, 2, 1]
+
+# Running simulated annealing
+params, h_list = simulated_annealing(init_params, dt, data_xy, T_precision=10**2)
+
+# Plotting approx predator prey
+simulated_data = pred_prey(params, 100, dt)
+
+# plotting results
+plt.plot(data_t, simulated_data)
+plt.show()
+
+plt.plot(h_list)
+plt.show()
+
+
